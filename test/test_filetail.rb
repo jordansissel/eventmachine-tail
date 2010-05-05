0719b3796df17eaf6b4ff09b862b3a187f2c402c
Jordan Sissel <jls@semicomplete.com>
2010-05-05 02:16:24 -0700

    - Use randomly-generated data - Track line numbers - Use an EventMachine-ey way of aborting on timeouts rather than using   the Timeout module. (see abort_after_timeout)

--- test/test_filetail.rb
+++ test/test_filetail.rb
@@ -1,11 +1,15 @@
 #!/usr/bin/env ruby
 
-$:.unshift "../lib"
 require 'rubygems'
+$:.unshift "#{File.dirname(__FILE__)}/../lib"
 require 'eventmachine-tail'
+require 'tempfile'
 require 'test/unit'
+require 'timeout'
 
-DATA = (1000..1010).to_a.collect { |i| i.to_s }
+
+# Generate some data
+DATA = (1..10).collect { |i| rand.to_s }
 SLEEPMAX = 2
 
 class Reader < EventMachine::FileTail
@@ -14,13 +18,15 @@ class Reader < EventMachine::FileTail
     @data = DATA.clone
     @buffer = BufferedTokenizer.new
     @testobj = testobj
+    @lineno = 0
   end # def initialize
 
   def receive_data(data)
     @buffer.extract(data).each do |line|
+      @lineno += 1
       expected = @data.shift
-      puts expected
-      @testobj.assert_equal(expected, line)
+      @testobj.assert_equal(expected, line, 
+          "Expected '#{expected}' on line #{@lineno}, but got '#{line}'")
       if @data.length == 0
         EM.stop_event_loop
       end
@@ -29,44 +35,43 @@ class Reader < EventMachine::FileTail
 end # class Reader
 
 class TestFileTail < Test::Unit::TestCase
+  def abort_after_timeout(seconds)
+    EM::Timer.new(seconds) do
+      EM.stop_event_loop
+      flunk("Timeout (#{seconds} seconds) while running tests. Failing.")
+    end
+  end
 
   # This test should run slow. We are trying to ensure that
   # our file_tail correctly reads data slowly fed into the file
   # as 'tail -f' would.
   def test_filetail
-    require 'tempfile'
-    require 'timeout'
+    tmp = Tempfile.new("testfiletail")
+    data = DATA.clone
+    EM.run do
+      abort_after_timeout(DATA.length * SLEEPMAX + 10)
 
-    Timeout.timeout(DATA.length * SLEEPMAX + 10) do
-      tmp = Tempfile.new("testfiletail")
-      data = DATA.clone
-      EM.run do
-        EM::file_tail(tmp.path, Reader, -1, self)
-          
-        timer = EM::PeriodicTimer.new(0.2) do
-          tmp.puts data.shift
-          tmp.flush
-          sleep(rand * SLEEPMAX)
-          timer.cancel if data.length == 0
-        end
-      end # EM.run
-    end # Timeout.timeout
+      EM::file_tail(tmp.path, Reader, -1, self)
+      timer = EM::PeriodicTimer.new(0.2) do
+        tmp.puts data.shift
+        tmp.flush
+        sleep(rand * SLEEPMAX)
+        timer.cancel if data.length == 0
+      end
+    end # EM.run
   end # def test_filetail
 
   def test_filetail_with_seek
-    require 'tempfile'
-    require 'timeout'
+    tmp = Tempfile.new("testfiletail")
+    data = DATA.clone
+    data.each { |i| tmp.puts i }
+    tmp.flush
+    EM.run do
+      abort_after_timeout(2)
 
-    Timeout.timeout(2) do
-      tmp = Tempfile.new("testfiletail")
-      data = DATA.clone
-      data.each { |i| tmp.puts i }
-      tmp.flush
-      EM.run do
-        # Set startpos of 0
-        EM::file_tail(tmp.path, Reader, 0, self)
-      end # EM.run
-    end # Timeout.timeout
+      # Set startpos of 0 (beginning of file)
+      EM::file_tail(tmp.path, Reader, 0, self)
+    end # EM.run
   end # def test_filetail
 end # class TestFileTail
 
